14a12b20b15979c510d7e628c4cbf76ad9645af6
thudsonbu <hudson_thom@bentley.edu>
2020-10-20 13:21:19 -0400

    Name counts and common names read from file and working correctly

---
+++ Assignment2/nameQuestions.php
@@ -0,0 +1,76 @@
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap Style Sheets -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- Custom Font -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400" rel="stylesheet">
+
+    <!-- Custom Styles -->
+    <styles type="text/css"> 
+
+
+
+    </styles>
+
+</head>
+
+<?php 
+
+// Read boy names
+$boyFile = fopen("C:\Users\Tom Hudson\Desktop\CS380\code\Assignment2\boynames.txt", "r");
+
+$boyNames = array();
+$boyTotal = 0;
+
+while(!feof($boyFile)) {
+
+    $line = fgets($boyFile);
+
+    if(isset($line)) {
+
+        $values = explode(" ", $line);
+        $name = $values[0];
+        $count = $values[1];
+
+        $boyNames += array($name => $count);
+        $boyTotal += intval($count);
+    }
+}
+
+fclose($boyFile);
+
+// Read girl names file
+
+$girlFile = fopen("C:\Users\Tom Hudson\Desktop\CS380\code\Assignment2\girlnames.txt", "r");
+
+$girlNames = array();
+$girlTotal = 0;
+
+while(!feof($girlFile)) {
+
+    $line = fgets($girlFile);
+
+    
+
+    if(isset($line))  {
+
+        $values = explode(" ", $line);
+        $name = $values[0];
+        $count = $values[1];
+
+        $girlNames += array( $name => $count );
+        $girlTotal += intval($count);
+    }
+}
+
+fclose($girlFile);
+
+$commonBoyNames = array_intersect_key( $boyNames, $girlNames );
+
+echo($commonBoyNames['Riley']);
+
+$commonGirlNames = array_intersect_key( $girlNames, $boyNames );
